0f4f0fe91d9c5a794cdb24ae7a4e3c27600dbd4b
Zach Tellman <ztellman@gmail.com>
2014-01-30 15:19:44 -0800

    update dependencies

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "peering down into the machine"
   :license {:name "MIT Public License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[potemkin "0.3.4-SNAPSHOT"]
+  :dependencies [[potemkin "0.3.4"]
                  [primitive-math "0.1.3"]
-                 [clj-tuple "0.1.2"]
-                 [byte-streams "0.1.5"]
-                 [riddley "0.1.5-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[criterium "0.4.1"]
+                 [clj-tuple "0.1.4"]
+                 [byte-streams "0.1.7"]
+                 [riddley "0.1.6"]]
+  :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [org.clojure/clojure "1.5.1"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
              :benchmark {:jvm-opts ["-Dvertigo.unsafe"]}}
