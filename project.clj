13c5a621ae473ed0315e02732057a323498959f5
Zach Tellman <ztellman@gmail.com>
2015-12-02 00:47:09 -0800

    mark 0.1.4

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject vertigo "0.1.4-SNAPSHOT"
+(defproject vertigo "0.1.4"
   :description "peering down into the machine"
   :license {:name "MIT Public License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[potemkin "0.3.11"]
+  :dependencies [[potemkin "0.4.2"]
                  [primitive-math "0.1.4"]
-                 [clj-tuple "0.1.7"]
-                 [byte-streams "0.2.0-alpha7"]
-                 [riddley "0.1.9"]]
+                 [clj-tuple "0.2.2"]
+                 [byte-streams "0.2.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
-                                  [org.clojure/clojure "1.5.1"]
+                                  [org.clojure/clojure "1.8.0-alpha4"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
              :benchmark {:jvm-opts ["-Dvertigo.unsafe"]}}
   :plugins [[codox "0.6.4"]]
